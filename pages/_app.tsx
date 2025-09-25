cd3f82b9aab5966bde265bda219677635f2407ed
Narkas Vaniurkina <nara@Mac.fritz.box>
2025-09-25 22:20:49 +0200

    testing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,5 @@
-// pages/_app.tsx
 import type { AppProps } from "next/app";
-import "../styles/globals.css"; // see next step
-
+import "../styles/globals.css"; // make sure this file exists
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
