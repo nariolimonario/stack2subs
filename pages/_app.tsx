cac8d6114e5095d4e23a7e1dc0a92c8b06dcd07f
Narkas Vaniurkina <nara@Mac.fritz.box>
2025-09-27 17:29:18 +0200

    add favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,28 @@
 // pages/_app.tsx
 import type { AppProps } from "next/app";
-import "../styles/globals.css"; // see next step
+import Head from "next/head"; // 👈 import Head
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon-96x96.png"
+          sizes="96x96"
+        />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
